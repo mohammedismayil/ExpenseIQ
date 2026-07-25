1fce0ce896d8c756a36b6662a18ece89128b4fef
Mohammed ismayil <mohammedismayil10@gmail.com>
2026-07-25 20:26:01 +0530

    Initial Commit

---
+++ ExpenseIQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseIQ
+//
+//  Created by Mohammed Ismayil on 25/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
