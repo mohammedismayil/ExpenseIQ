b5dcb6d95dfe682e653a5d02cc9c16fcc4864514
Mohammed ismayil <mohammedismayil10@gmail.com>
2026-07-25 21:56:40 +0530

    feat: add app color design system

---
+++ ExpenseIQ/DesignSystem/Colors/Appcolors.swift
@@ -0,0 +1,14 @@
+//
+//  Appcolors.swift
+//  ExpenseIQ
+//
+//  Created by Mohammed Ismayil on 25/07/26.
+//
+
+import UIKit
+
+enum Appcolors {
+    static let primary = UIColor.systemBlue
+    static let background = UIColor.systemBackground
+    static let textPrimary = UIColor.label
+}
